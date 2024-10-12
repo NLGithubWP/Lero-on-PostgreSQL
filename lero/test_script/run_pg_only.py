e2cc425c1b47de810c1f09d0421e0b8a6aae46d9
working <57171759+NLGithubWP@users.noreply.github.com>
2024-10-12 22:12:53 +0800

    update

--- lero/test_script/run_pg_only.py
+++ lero/test_script/run_pg_only.py
@@ -38,9 +38,10 @@ try:
         if query_id.startswith("q") and 1 <= int(query_id[1:]) <= 22:
             explain_query = f"EXPLAIN (ANALYZE, TIMING, VERBOSE, COSTS, SUMMARY, FORMAT JSON) {query}"
             cursor.execute(explain_query)
-            explain_result = cursor.fetchall()
-            execution_time = json.loads(explain_result[0][0])["Execution Time"]
-            # print(f"\nExplanation for {query_id}:\n", explain_result)
+            explain_result = cursor.fetchall()[0][0]
+            explain_data = json.loads(explain_result)
+            execution_time = explain_data[0]["Execution Time"]
+            # print(f"\nExplanation for {query_id}:\n", json.dumps(explain_data, indent=2))
             print(f"Execution Time for {query_id}: {execution_time} ms")
 
 except psycopg2.Error as e:
