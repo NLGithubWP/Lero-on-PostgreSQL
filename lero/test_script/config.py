5bdd9922af3860009dbab80534db3b2b3afd2fcf
working <57171759+NLGithubWP@users.noreply.github.com>
2024-10-14 14:45:01 +0800

    Update

--- lero/test_script/config.py
+++ lero/test_script/config.py
@@ -3,7 +3,7 @@ PORT = 5432
 HOST = "localhost"
 USER = "postgres"
 PASSWORD = "123"
-DB = "tpch1"
+DB = "tpch"
 CONNECTION_STR = "dbname=" + DB + " user=" + USER + " password=" + PASSWORD + " host=localhost port=" + str(PORT)
 TIMEOUT = 30000000
 # [important]
