e1de08e201017fd52c58734d5aea36c4fbbbe211
working <57171759+NLGithubWP@users.noreply.github.com>
2025-02-22 16:44:55 +0800

    Update code

--- lero/test_script/config.py
+++ lero/test_script/config.py
@@ -3,10 +3,10 @@ PORT = 5432
 HOST = "localhost"
 USER = "postgres"
 PASSWORD = "123"
-# DB = "imdb_ori"
+DB = "imdb_ori"
 # DB = "imdb_01v2"
 # DB = "imdb_05v2"
-DB = "imdb_07v2"
+# DB = "imdb_07v2"
 CONNECTION_STR = "dbname=" + DB + " user=" + USER + " password=" + PASSWORD + " host=localhost port=" + str(PORT)
 TIMEOUT = 180000
 # [important]
